75dda61851ace4d231e1a2f22d52b56ca7449384
Seamus Morrison <llamathedark@gmail.com>
2025-07-09 21:22:39 -0600

    new: allow player to pick up gizmo which the camera follows
    Does this by having a Marker2D on the player to mark a place for the marker to rest, and an Area2D on the gizmo to act as a trigger to mark that the player is in range to pick it up
    
    picking up the gizmo disables collision.
    
    careful when enabling position smoothing--it kinda freaks out. I assume we don\'t want that anyways.

---
+++ the-grand-gizmo-escape/Scripts/pickup_controller.gd
@@ -0,0 +1,92 @@
+extends Node
+
+class_name PickupController
+## Handles pickup and dropping of a "Gizmo" (RigidBody2D)
+
+@export var socket_path		: NodePath		# The hold socket on the player
+@export var pickup_area_path	: NodePath	# The path to the Area2D that defines which objects can grab the Gizmo
+@export var pickup_action	: String = "Use"
+@export var tween_time		: float = 0.15 # seconds for animation to fly to hand? Or we could just have it instant
+
+var held : RigidBody2D = null
+var candidates : Array[RigidBody2D] = []
+
+@onready var player : CharacterBody2D = get_parent()
+@onready var socket : Node2D = get_node(socket_path)
+@onready var area	: Area2D = get_node(pickup_area_path)
+
+
+## INITIALIZATION
+func _ready() -> void:
+	area.body_entered.connect(onBodyEntered)
+	area.body_exited.connect(onBodyExited)
+
+## HANDLE INPUT
+func _unhandled_input(event: InputEvent) -> void:
+	if (event.is_action_pressed(pickup_action)):
+		drop() if held else tryPickup()
+
+## PICKUP AREA SIGNALS
+func onBodyEntered(body: Node) -> void:
+	if (body is RigidBody2D and body.is_in_group('pickable')): # TODO: make group 'pickable'
+		candidates.append(body)
+
+func onBodyExited(body: Node) -> void:
+	if (body is RigidBody2D):
+		candidates.erase(body)
+
+## PICK-UP AND DROP LOGIC
+func tryPickup() -> void:
+	if (candidates.is_empty()):
+		return
+	candidates.sort_custom(compareDistance) # I know we won't have more than one gizmo but maybe?
+	pickup(candidates[0])
+
+func compareDistance(a, b) -> bool:
+	return	a.global_position.distance_to(player.global_position) \
+		  < b.global_position.distance_to(player.global_position)
+
+func pickup(gizmo : RigidBody2D) -> void:
+	held = gizmo
+	
+	# disable physics for gizmo
+	gizmo.freeze = true
+	gizmo.freeze_mode = RigidBody2D.FREEZE_MODE_STATIC # TODO: correct mode?
+	gizmo.sleeping = true
+	gizmo.linear_velocity = Vector2.ZERO
+	gizmo.angular_velocity = 0.0
+	
+	for child in gizmo.get_children():
+		if (child is CollisionShape2D):
+			child.disabled = true
+	
+	# "tween" is a word I just learned. don't know how I feel about it
+	var tween := create_tween()
+	tween.tween_property(
+		gizmo, "global_position", socket.global_position, tween_time
+	).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	
+	tween.tween_callback(Callable(self, "attachToSocket").bind(gizmo))
+
+func attachToSocket(gizmo: RigidBody2D) -> void:
+	gizmo.reparent(socket)
+	gizmo.position = Vector2.ZERO # aligns gizmo exactly with the socket
+
+func drop() -> void:
+	var gizmo := held
+	held = null
+	if not gizmo:
+		return
+	
+	gizmo.reparent(get_tree().current_scene) # release the gizmo back into the wild
+	gizmo.global_rotation = 0
+	
+	# enable physics for gizmo
+	gizmo.freeze = false
+	gizmo.sleeping = false
+	
+	for child in gizmo.get_children():
+		if (child is CollisionShape2D):
+			child.disabled = false
+	
+	gizmo.apply_impulse(Vector2.ZERO, Vector2.DOWN * 400)
--- the-grand-gizmo-escape/Scripts/player.gd
+++ the-grand-gizmo-escape/Scripts/player.gd
@@ -1,20 +1,20 @@
 extends CharacterBody2D
 
-const SPEED = 600.0
-const ACCELERATION = 1200.0
-const FRICTION = 1400.0
+@export var SPEED = 600.0
+@export var ACCELERATION = 1200.0
+@export var FRICTION = 1400.0
 
-const GRAVITY = 2000.0
-const FALL_GRAVITY = 3000.0
-const FAST_FALL_GRAVITY = 5000.0
-const WALL_GRAVITY = 25.0
+@export var GRAVITY = 2000.0
+@export var FALL_GRAVITY = 3000.0
+@export var FAST_FALL_GRAVITY = 5000.0
+@export var WALL_GRAVITY = 25.0
 
-const JUMP_VELOCITY = -700.0
-const WALL_JUMP_VELOCITY = -800.0
-const WALL_JUMP_PUSHBACK = 700.0 # 300.0
+@export var JUMP_VELOCITY = -700.0
+@export var WALL_JUMP_VELOCITY = -800.0
+@export var WALL_JUMP_PUSHBACK = 700.0 # 300.0
 
-const INPUT_BUFFER_PATIENCE = 0.1
-const COYOTE_TIME = 0.08
+@export var INPUT_BUFFER_PATIENCE = 0.1
+@export var COYOTE_TIME = 0.08
 
 var input_buffer : Timer
 var coyote_timer : Timer
