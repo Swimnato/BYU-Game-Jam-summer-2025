7c8241c6153ba65d571f29e985f603390d70f193
Seamus Morrison <llamathedark@gmail.com>
2025-07-09 19:25:59 -0600

    Complete basic movement demo
    includes:
    - LR/Jump
    - Wall jump

---
+++ the-grand-gizmo-escape/Scripts/player.gd
@@ -0,0 +1,87 @@
+extends CharacterBody2D
+
+const SPEED = 600.0
+const ACCELERATION = 1200.0
+const FRICTION = 1400.0
+
+const GRAVITY = 2000.0
+const FALL_GRAVITY = 3000.0
+const FAST_FALL_GRAVITY = 5000.0
+const WALL_GRAVITY = 25.0
+
+const JUMP_VELOCITY = -700.0
+const WALL_JUMP_VELOCITY = -800.0
+const WALL_JUMP_PUSHBACK = 700.0 # 300.0
+
+const INPUT_BUFFER_PATIENCE = 0.1
+const COYOTE_TIME = 0.08
+
+var input_buffer : Timer
+var coyote_timer : Timer
+var coyote_jump_available : bool = true
+
+func _ready():
+	# Setup input buffer timer
+	input_buffer = Timer.new()
+	input_buffer.wait_time = INPUT_BUFFER_PATIENCE
+	input_buffer.one_shot = true
+	add_child(input_buffer)
+	
+	# Setup coyote timer
+	coyote_timer = Timer.new()
+	coyote_timer.wait_time = COYOTE_TIME
+	coyote_timer.one_shot = true
+	add_child(coyote_timer)
+	coyote_timer.timeout.connect(coyoteTimeout)
+
+func _physics_process(delta: float) -> void:
+	var horizontal_input = Input.get_axis("Left", "Right")
+	var jump_attempted = Input.is_action_just_pressed("Jump")
+	
+	# Handle jumping
+	if (jump_attempted or input_buffer.time_left > 0):
+		if (coyote_jump_available):
+			velocity.y = JUMP_VELOCITY
+			coyote_jump_available = false
+		elif (is_on_wall() and horizontal_input != 0):
+			velocity.y = WALL_JUMP_VELOCITY
+			velocity.x = WALL_JUMP_PUSHBACK * -sign(horizontal_input)
+		elif (jump_attempted):
+			input_buffer.start()
+	
+	# Fall faster when you don't hold jump key
+	if (Input.is_action_just_released("Jump") and velocity.y < 0):
+		velocity.y = JUMP_VELOCITY / 4
+	
+	if (is_on_floor()):
+		coyote_jump_available = true
+		coyote_timer.stop()
+	else:
+		if coyote_jump_available:
+			if coyote_timer.is_stopped():
+				coyote_timer.start()
+		velocity.y += getGravity(horizontal_input) * delta
+		
+	# Horizontal motion
+	var floor_damping : float = 1.0 if is_on_floor() else 0.2
+	# var dash_multiplier : float 2.0 if Input.ison_pressed("Dash")
+	if (horizontal_input):
+		velocity.x = move_toward(velocity.x, horizontal_input * SPEED, ACCELERATION * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, (FRICTION * delta) * floor_damping)
+		
+	move_and_slide()
+
+
+func getGravity(input_dir : float = 0) -> float:
+	# if Input.is_action_pressed("FastFall")
+	#	return FAST_FALL_GRAVITY
+	if is_on_wall_only() and velocity.y > 0 and input_dir != 0:
+		return WALL_GRAVITY
+	return GRAVITY if (velocity.y < 0) else FALL_GRAVITY
+	
+
+func coyoteTimeout():
+	# reset coyote jump when you timeout
+	coyote_jump_available = false
+	
