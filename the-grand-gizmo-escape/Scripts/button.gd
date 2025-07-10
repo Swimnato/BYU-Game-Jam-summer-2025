906bc0fe667e3a701c282dfd39eb59968a9522c0
Bubadabado <adam@adamrobertson.us>
2025-07-10 08:31:47 -0600

    buttons

---
+++ the-grand-gizmo-escape/Scripts/button.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var door = Node
+
+var on := false
+var other_body: Node2D
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is RigidBody2D or body.name == "Player":
+		on = true
+		other_body = body
+		door.setState(on)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body == other_body:
+		on = false
+		door.setState(on)
