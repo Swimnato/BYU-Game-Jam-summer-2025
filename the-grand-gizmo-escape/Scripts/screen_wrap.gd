66ebd20fb03cdddfef42b4478b77d129a55162a4
Swimnato <66497456+Swimnato@users.noreply.github.com>
2025-07-11 23:21:22 -0600

    Preliminary Code

--- the-grand-gizmo-escape/Scripts/screen_wrap.gd
+++ the-grand-gizmo-escape/Scripts/screen_wrap.gd
@@ -6,12 +6,23 @@ var wrapEnabled : bool = true;
 
 const scaleFactor : Vector2 = Vector2(1920.0, 1080.0); #This is the native resolution of the scene
 
+var solidAreas : Array[Vector2];
+
 func _ready() -> void:
 	scale = (Vector2(get_viewport().size) / scaleFactor) / $"../GizmoCam".zoom;
 	GlobalVars.gizmoCamPTR = gizmoCam;
 
 func _process(delta: float) -> void:
 	rotation = - $"..".rotation;
+	solidAreas = checkForCollisionsBetweenSides(global_position + scaleFactor/2 * Vector2(1,-1), global_position + scaleFactor/2 );
+	queue_redraw();
+
+func _draw() -> void:
+	if(len(solidAreas) == 0):
+		return;
+	for area in solidAreas:
+		print(area); 
+		draw_rect(Rect2(Vector2(scaleFactor.x/2 - 10, to_local(area).y),Vector2(scaleFactor.x/2 + 10, to_local(Vector2(0, area.x)).y)), Color.RED);
 
 func _on_in_camera_body_exited(body: Node2D) -> void:
 	if(body.name == "Player"):
@@ -70,3 +81,64 @@ func _on_left_hitbox_body_entered(body: Node2D) -> void:
 func _on_right_hitbox_body_entered(body: Node2D) -> void:
 	if(body.name == "Player"):
 		body.addClone(scaleFactor * Vector2(-1,0) + Vector2(-1,0));
+
+func checkForCollisionsBetweenSides(point_a : Vector2, point_b : Vector2):
+	var returnVal: Array[Vector2];
+	var bodiesFound: Array;
+	
+	var space = get_world_2d().direct_space_state;
+	var searchingForObjects = true;
+	
+	#find bottom coord
+	while(searchingForObjects):
+		var query = PhysicsRayQueryParameters2D.create(point_a, point_b);
+		query.exclude = bodiesFound;
+		var resultsFromCast = space.intersect_ray(query);
+		if(!resultsFromCast):
+			searchingForObjects = false;
+		else:
+			bodiesFound.append(resultsFromCast.rid);
+			if(point_a.x == point_b.x):
+				returnVal.append(Vector2(resultsFromCast.position.y, 0));
+			elif(point_a.y == point_b.y):
+				returnVal.append(Vector2(resultsFromCast.position.x, 0));
+	
+	#check from the opposite direction (if you start a raycast in the middle of an object it won't detect it)
+	while(searchingForObjects):
+		var query = PhysicsRayQueryParameters2D.create(point_b, point_a);
+		query.exclude = bodiesFound;
+		var resultsFromCast = space.intersect_ray(query);
+		if(!resultsFromCast):
+			searchingForObjects = false;
+		else:
+			bodiesFound.append(resultsFromCast.rid);
+			if(point_a.x == point_b.x):
+				returnVal.append(Vector2(point_a.y, resultsFromCast.position.y));
+			elif(point_a.y == point_b.y):
+				returnVal.append(Vector2(point_a.x, resultsFromCast.position.x));
+	
+	#find top coords of first found bodies
+	for body in range(len(bodiesFound)):
+		if(returnVal[body].y == 0):
+			var exclusionList = bodiesFound.duplicate(true);
+			exclusionList.remove_at(body);
+			var query = PhysicsRayQueryParameters2D.create(point_b, point_a);
+			query.exclude = exclusionList;
+			var resultsFromCast = space.intersect_ray(query);
+			if(resultsFromCast):
+				if(point_a.x == point_b.x):
+					returnVal[body].y = resultsFromCast.position.y;
+				elif(point_a.y == point_b.y):
+					returnVal[body].y = resultsFromCast.position.x;
+			else:
+				if(point_a.x == point_b.x):
+					returnVal[body].y = point_b.y;
+				elif(point_a.y == point_b.y):
+					returnVal[body].y = point_b.x;
+	
+	return returnVal;
+
+
+
+
+var h;
