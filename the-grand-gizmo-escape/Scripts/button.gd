dab9f3874c2b862af821cc3595f66d4a91bc2701
Bubadabado <adam@adamrobertson.us>
2025-07-13 17:19:48 -0600

    fix button bug, add in new wip tileset

--- the-grand-gizmo-escape/Scripts/button.gd
+++ the-grand-gizmo-escape/Scripts/button.gd
@@ -3,21 +3,24 @@ extends Node2D
 @export var door = Node
 
 var on := false
-var other_body: Node2D
+var other_bodies: Array[Node2D]
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is RigidBody2D or body.name == "Player":
+	if (body is RigidBody2D or body.name == "Player") and !(body in other_bodies):
 		on = true
-		other_body = body
+		other_bodies.append(body)
 		updateDoor()
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body == other_body:
+	if body in other_bodies:
+		other_bodies.erase(body)
+	if other_bodies.is_empty():
 		on = false
 		updateDoor()
 
 func reset():
 	on = false
+	other_bodies.clear()
 	updateDoor()
 	
 func updateDoor():
