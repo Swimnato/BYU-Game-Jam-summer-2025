df34010be7cde0306076bdb5061b8e699f64bc90
Swimnato <66497456+Swimnato@users.noreply.github.com>
2025-07-12 23:38:59 -0600

    Menu updated

--- the-grand-gizmo-escape/Scripts/main_scene.gd
+++ the-grand-gizmo-escape/Scripts/main_scene.gd
@@ -1,34 +1,69 @@
 extends Node2D
 
 var countup : float = 0.0;
+var timeSinceBlinkText = 0.0;
 var explosionHappened : bool = false;
+var bldngRasiated : bool = false;
 var menuAppeared : bool = false;
 @onready var gameScn = preload("res://Scenes/testing/MovementDemo.tscn");
 @onready var creditsScn = preload("res://Scenes/UI/Credits.tscn");
 
 @onready var explodedTexture = preload("res://Assets/The_explosion.png");
+@onready var postExplodedTexture = preload("res://Assets/rasiated.png");
 
 @onready var prisionBuildingSprite = $"Prision Building";
-@onready var menuButtons = $Main_Menu/Menu_Buttons;
-@onready var menuCam = $Main_Menu/Main_Menu_Cam;
+@onready var menuButtons = $Main_Menu;
+@onready var menuCam = $Main_Menu_Cam;
+@onready var blowupTxt = $"Blowup Text";
+@onready var playBtn = $"Main_Menu/Menu_Buttons/Play Button";
 
-@export var startToExplosionDelay = 2;
-@export var startToMenuDelay = 4;
+@export var startToRasiatedDelay = 0.5;
+@export var startToMenuDelay = 2;
+@export var blinkDuration = 1.5;
+
+@export var mainMenuBegRot : float = 20;
+var mainMenuEndRot : float;
+@export var mainMenuBegPos : Vector2 = Vector2(30,20);
+var mainMenuEndPos : Vector2;
+@export var mainMenuBegScale: Vector2 = Vector2(.1,.1);
+var mainMenuEndScale : Vector2;
 
 func _ready() -> void:
 	menuButtons.visible = false;
+	blowupTxt.visible = true;
+	mainMenuEndRot = menuButtons.rotation;
+	mainMenuEndPos = menuButtons.position;
+	mainMenuEndScale = menuButtons.scale;
 
 func _process(delta: float) -> void:
-	if(!explosionHappened or !menuAppeared):
+	if(explosionHappened):
 		countup += delta;
-		if(countup >= startToExplosionDelay and !explosionHappened):
-			prisionBuildingSprite.texture = explodedTexture;
-			explosionHappened = true;
+		if(countup > startToRasiatedDelay and !bldngRasiated):
+			bldngRasiated = true;
+			prisionBuildingSprite.texture = postExplodedTexture;
+			menuButtons.visible = true;
+			menuButtons.rotation = mainMenuBegRot;
+			menuButtons.position = mainMenuBegPos;
+			menuButtons.scale = mainMenuBegScale;
+			var tween : Tween = self.create_tween()
+			tween.set_parallel()
+			tween.tween_property(menuButtons, "position", mainMenuEndPos, startToMenuDelay);
+			tween.tween_property(menuButtons, "rotation", mainMenuEndRot, startToMenuDelay);
+			tween.tween_property(menuButtons, "scale", mainMenuEndScale, startToMenuDelay);
 		elif(countup >= startToMenuDelay and !menuAppeared):
 			menuAppeared = true;
-			menuButtons.visible = true;
-			
+			playBtn.grab_focus();
+	else:
+		timeSinceBlinkText += delta;
+		if(timeSinceBlinkText > blinkDuration):
+			timeSinceBlinkText = 0.0;
+			blowupTxt.visible = !blowupTxt.visible;
 
+func _unhandled_input(event: InputEvent) -> void:
+	if (event.is_action_pressed("Use") and !explosionHappened):
+		prisionBuildingSprite.texture = explodedTexture;
+		explosionHappened = true;
+		blowupTxt.visible = false;
 
 func _on_play_button_pressed() -> void:
 	menuButtons.visible = false;
