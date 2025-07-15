889d8daa30880fd51eb487a995e294ec98218bec
Bubadabado <adam@adamrobertson.us>
2025-07-14 18:02:55 -0600

    button and lever animations

--- the-grand-gizmo-escape/Scripts/door.gd
+++ the-grand-gizmo-escape/Scripts/door.gd
@@ -3,7 +3,10 @@ extends Node2D
 @onready var collision := $StaticBody2D/CollisionShape2D
 @onready var sprite := $Sprite2D
 
-var open := false
+var open
+
+func _onready():
+	open = false;
 
 func setState(to):
 	open = to
--- the-grand-gizmo-escape/Scripts/lever.gd
+++ the-grand-gizmo-escape/Scripts/lever.gd
@@ -4,14 +4,24 @@ extends Node2D
 
 var on := false
 
+func _onready():
+	reset()
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		on = !on
 		updateDoor()
+		if on:
+			$Sprite2D/AnimationPlayer.play("swing")
+		else: 
+			$Sprite2D/AnimationPlayer.play("swing", -1, -1, true)
 
 func reset():
+	$Sprite2D/AnimationPlayer.play("RESET")
 	on = false
 	updateDoor()
 	
 func updateDoor():
 	door.setState(on)
+
+		
