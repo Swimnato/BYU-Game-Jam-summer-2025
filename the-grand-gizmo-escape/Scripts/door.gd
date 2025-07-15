f3b216b831fe95fdec8f7dbb17ea3d55a82dd686
Swimnato <66497456+Swimnato@users.noreply.github.com>
2025-07-15 17:32:48 -0600

    More Level Progress

--- the-grand-gizmo-escape/Scripts/door.gd
+++ the-grand-gizmo-escape/Scripts/door.gd
@@ -14,14 +14,14 @@ func _onready():
 	open = false;
 
 func setState(to):
-	open = to
-	collision.set_deferred("disabled", open)
-	if(open):
+	collision.set_deferred("disabled", to);
+	if(!open && to):
 		$Sprite2D/AnimationPlayer.play("open")
 		position = positionOriginal + offsetOn;
-	else:
+	elif(open && !to):
 		$Sprite2D/AnimationPlayer.play("open", -1, -1, true)
 		position = positionOriginal;
+	open = to;
 	#sprite.visible = !open
 
 func reset():
