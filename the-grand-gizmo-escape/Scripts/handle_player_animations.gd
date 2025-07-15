9150e9289db78a2de9f9d54f7c51b27971f0aed8
Bubadabado <adam@adamrobertson.us>
2025-07-14 23:04:26 -0600

    most animations done

--- the-grand-gizmo-escape/Scripts/handle_player_animations.gd
+++ the-grand-gizmo-escape/Scripts/handle_player_animations.gd
@@ -1,16 +1,32 @@
 extends Sprite2D
 
 var default_scale
+var jumped
 
 func _ready():
+	jumped = false
 	default_scale = self.scale.x
 
 func face(dir):
 	self.scale.x = -default_scale if dir < 0 else default_scale if dir > 0 else self.scale.x
 
-func handleAnimation(velocity, jumping):
+func handleAnimation(velocity, jumping, on_wall):
 	face(velocity.x)
 	if abs(velocity.x) > 0 and !jumping:
 		$AnimationPlayer.play("walk")
+		jumped = false
+	elif on_wall:
+		$AnimationPlayer.play("wall_slide")
+	elif jumping:
+		if velocity.y < 0:
+			$AnimationPlayer.play("jump", -1, 0.5)
+		else:
+			$AnimationPlayer.play("fall")
+		if !jumped:
+			jumped = true
+	elif abs(velocity.x) == 0:
+		$AnimationPlayer.play("idle")
+		jumped = false
 	else:
 		$AnimationPlayer.stop()
+		jumped = false
